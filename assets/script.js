fafd74637fb3394b5585de746fdc9b33174771d9
MATEUS FARIAS SANTOS <tereus72@gmail.com>
2024-02-14 21:51:35 -0300

    commit hora

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,22 @@ function alterarCores() {
         text1.style.backgroundColor = "#D3D3D3";
     }
 }
+// Função que mostra a hora atual na página
+function mostrarHora() {
+    // Cria um objeto Date com a hora atual
+    var agora = new Date();
+    // Formata a hora de acordo com o fuso horário do Brasil
+    var hora = agora.toLocaleString("pt-BR", {timeZone: "America/Sao_Paulo"});
+    // Seleciona o elemento div com o id "hora"
+    var div = document.getElementById("hora");
+    // Altera o conteúdo do elemento div com a hora formatada
+    div.innerHTML = hora;
+  }
+  
+  // Função que é executada quando o usuário clica no botão
+  function clicarBotao() {
+    // Chama a função mostrarHora uma vez
+    mostrarHora();
+    // Cria um intervalo que chama a função mostrarHora a cada 1000 milissegundos (1 segundo)
+    setInterval(mostrarHora, 1000);
+  }
